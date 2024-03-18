3bd8ae1f3a532eda09a4cd1e7ddde552fb67348d
Roaming Debug <ezrage2001@outlook.com>
2024-03-18 17:29:22 -0500

    updated the layout

--- app/src/main/java/com/example/hiringdisplay/MainActivity.java
+++ app/src/main/java/com/example/hiringdisplay/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Features to be added", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
